386a43cc12d31902d09b9c88fc48dbf3e8d7c03a
Youngje Yun <youngje.yun@gmail.com>
2012-10-26 10:55:52 +0900

    Add n4upgrade java project

---
+++ n4Upgrade/src/Upgrade.java
@@ -0,0 +1,13 @@
+
+public class Upgrade {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("sssss");
+		Upgrader upgrader = new Upgrader();
+		upgrader.upgrade();
+	}
+
+}
